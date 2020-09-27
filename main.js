de5cdf667eba967a28a33f6dfb609fde5b992113
William Mongan <billmongan@gmail.com>
2020-09-26 23:45:39 -0400

    Formatting missing status clearing json body

--- main.js
+++ main.js
@@ -365,7 +365,7 @@ const serverHandler = (req, res) => {
 
                                         let missingurl = "/api/v1/courses/"+CANVAS_COURSE_ID[i]+"/assignments/" + asmtid[asmtidx] + "/submissions/" + user_id;
                                         console.log("Clearing missing status, if any: " + missingurl);
-                                        let missingbody = "{ \"submission\": { \"late_policy_status\": \"none\" } }";
+                                        let missingbody = { "submission": { "late_policy_status": "none" } }; 
                                         httprequestCanvas(missingurl, 443, "PUT", missingbody, {"Authorization": "Bearer " + CANVAS_API_KEY}, printResp);
                                     } catch(err) {
                                         console.log("Error Posting to Canvas: " + err.message);
